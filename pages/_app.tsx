a991d335298637ee637025e4bc73eae4f3afa25c
helloheesu <heesu.jung.developer@gmail.com>
2022-02-07 23:31:23 +0900

    fix 'useLayoutEffect' warning for SSR

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,11 +1,13 @@
 import '../styles/globals.css';
 import type { AppProps } from 'next/app';
-import { useLayoutEffect, useState } from 'react';
+import { useEffect, useState } from 'react';
 
 function MyApp({ Component, pageProps }: AppProps) {
   const [height, setHeight] = useState('100vh');
-  useLayoutEffect(() => {
-    setHeight(`${window.innerHeight}px`);
+  useEffect(() => {
+    if (typeof window !== 'undefined') {
+      setHeight(`${window.innerHeight}px`);
+    }
   }, []);
   return <Component {...pageProps} height={height} />;
 }
