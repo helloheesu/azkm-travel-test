ea3bcbd408ae1c01b1e076671c9dde4f1a9f45cb
helloheesu <heesu.jung.developer@gmail.com>
2022-02-11 01:45:20 +0900

    progressbar bar color only

--- components/Progress.tsx
+++ components/Progress.tsx
@@ -4,7 +4,18 @@ interface Props {
 }
 
 const Progress = ({ max, value }: Props) => {
-  return <progress max={max} value={value}></progress>;
+  const ratio = value / max;
+  return (
+    <div
+      className={`progress-container image-background ${
+        value === max ? 'end' : ''
+      }`}
+    >
+      <div className="border-container">
+        <div className="bar" style={{ width: `${ratio * 100}%` }}></div>
+      </div>
+    </div>
+  );
 };
 
 export default Progress;
