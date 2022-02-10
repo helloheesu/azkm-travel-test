fded6af46ef4cd5aef3e9f722a886a7603280351
helloheesu <heesu.jung.developer@gmail.com>
2022-02-10 16:13:16 +0900

    favicon on mobile
    manifest was not necessary but just in case.

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -19,7 +19,14 @@ function MyApp({ Component, pageProps }: AppProps) {
           name="description"
           content="나와 가장 잘 맞는 여행 스타일의 캐릭터는?"
         />
-        <link rel="icon" href="/favicon.ico" />
+        <link rel="apple-touch-icon" sizes="192x192" href="/favicon.png" />
+        <link rel="icon" type="image/png" href="/favicon.png" sizes="192x192" />
+        {/* IE */}
+        <link rel="shortcut icon" type="image/x-icon" href="/favicon.ico" />
+        {/* other browsers */}
+        <link rel="icon" type="image/x-icon" href="/favicon.ico" />
+        <meta name="msapplication-TileImage" content="/favicon.png" />
+        <meta name="msapplication-TileColor" content="#f5d57e"></meta>
       </Head>
       <Component {...pageProps} height={height} />
       <div className="image-preloader" />
