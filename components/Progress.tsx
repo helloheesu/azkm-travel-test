3f44100ab916efb4c30875f1d5de95deb2a73a51
helloheesu <heesu.jung.developer@gmail.com>
2022-02-11 03:15:59 +0900

    flatten the right edge of progress bar

--- components/Progress.tsx
+++ components/Progress.tsx
@@ -8,20 +8,22 @@ const Progress = ({ max, value }: Props) => {
   const percentage = (value / max) * 100;
 
   return (
-    <div
-      className={`progress-container image-background ${
-        value === max ? 'end' : ''
-      }`}
-    >
-      <div className="border-container">
-        <div className="bar" style={{ width: `${percentage}%` }} />
+    <div className="progress-wrapper">
+      <div
+        className={`progress-container image-background ${
+          value === max ? 'end' : ''
+        }`}
+      >
+        <div className="border-container">
+          <div className="bar" style={{ width: `${percentage}%` }} />
+        </div>
+        <img
+          className="indicator"
+          src="/images/progress/indicator.png"
+          alt=""
+          style={{ left: `${percentage - 8}%` }}
+        />
       </div>
-      <img
-        className="indicator"
-        src="/images/progress/indicator.png"
-        alt=""
-        style={{ left: `${percentage - 8}%` }}
-      />
     </div>
   );
 };
