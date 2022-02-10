65513420a24de3049ca277cef2879af32b22d61f
helloheesu <heesu.jung.developer@gmail.com>
2022-02-10 16:29:05 +0900

    og test

--- src/components/Head.tsx
+++ src/components/Head.tsx
@@ -3,15 +3,17 @@ import Head from 'next/head';
 interface Props {
   title?: string;
   description?: string;
+  img?: string;
 }
-const Comp = ({ title, description }: Props) => {
+const Comp = ({
+  title = '아찌끄미 여행 유형 테스트',
+  description = '나와 가장 잘 맞는 여행 스타일의 캐릭터는?',
+  img = '/og.png',
+}: Props) => {
   return (
     <Head>
-      <title>{title || '아찌끄미 여행 유형 테스트'}</title>
-      <meta
-        name="description"
-        content={description || '나와 가장 잘 맞는 여행 스타일의 캐릭터는?'}
-      />
+      <title>{title}</title>
+      <meta name="description" content={description} />
       <link rel="apple-touch-icon" sizes="192x192" href="/favicon.png" />
       <link rel="icon" type="image/png" href="/favicon.png" sizes="192x192" />
       {/* IE */}
@@ -20,6 +22,9 @@ const Comp = ({ title, description }: Props) => {
       <link rel="icon" type="image/x-icon" href="/favicon.ico" />
       <meta name="msapplication-TileImage" content="/favicon.png" />
       <meta name="msapplication-TileColor" content="#f5d57e"></meta>
+      <meta property="og:image" content={img} />
+      <meta property="og:title" content={title} />
+      <meta property="og:description" content={description} />
     </Head>
   );
 };
