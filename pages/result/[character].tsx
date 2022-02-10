c2e94f43b979440c41ee5de543dc9950b1064686
helloheesu <heesu.jung.developer@gmail.com>
2022-02-11 05:59:46 +0900

    add description on result page

--- pages/result/[character].tsx
+++ pages/result/[character].tsx
@@ -1,12 +1,15 @@
 import LoadingResultPage from 'components/LoadingResultPage';
 import ResultPage from 'components/ResultPage';
+import { Character } from 'data/character';
+import results from 'data/results';
 import { NextPage } from 'next';
 import { useRouter } from 'next/router';
 import React, { useEffect, useState } from 'react';
 
 const Result: NextPage = () => {
   const router = useRouter();
-  const character = router.query.character as string;
+  const character = router.query.character as Character;
+  const descriptions = results[character];
 
   const [isLoading, setIsLoading] = useState(true);
 
@@ -19,7 +22,7 @@ const Result: NextPage = () => {
   if (isLoading) {
     return <LoadingResultPage />;
   } else {
-    return <ResultPage character={character} />;
+    return <ResultPage character={character} descriptions={descriptions} />;
   }
 };
 
