39ad5acdfb47e02d6fadd545fe6fd77fc580fa68
helloheesu <heesu.jung.developer@gmail.com>
2022-02-22 11:49:00 +0900

    lowercase url

--- src/data/character.ts
+++ src/data/character.ts
@@ -2,13 +2,13 @@ import { i18nText } from './languages';
 
 // order matters, as it is the tie breaker
 export const characters = [
-  'Vanilla',
-  'Munboo',
-  'Totti',
-  'Somsom',
-  'Hiff',
-  'Cinamon',
-  'Coby',
+  'vanilla',
+  'munboo',
+  'totti',
+  'somsom',
+  'hiff',
+  'cinamon',
+  'coby',
 ] as const;
 
 export type Character = typeof characters[number];
@@ -16,11 +16,11 @@ export type Character = typeof characters[number];
 export const characterNames: {
   [key in Character]: i18nText;
 } = {
-  Totti: { ko: '또띠', en: 'Totti' },
-  Munboo: { ko: '먼부', en: 'Munboo' },
-  Vanilla: { ko: '바닐라', en: 'Vanilla' },
-  Cinamon: { ko: '시나몽', en: 'Cinamon' },
-  Coby: { ko: '코비', en: 'Coby' },
-  Hiff: { ko: '히프', en: 'Hiff' },
-  Somsom: { ko: '솜솜이', en: 'Somsom' },
+  totti: { ko: '또띠', en: 'Totti' },
+  munboo: { ko: '먼부', en: 'Munboo' },
+  vanilla: { ko: '바닐라', en: 'Vanilla' },
+  cinamon: { ko: '시나몽', en: 'Cinamon' },
+  coby: { ko: '코비', en: 'Coby' },
+  hiff: { ko: '히프', en: 'Hiff' },
+  somsom: { ko: '솜솜이', en: 'Somsom' },
 };
