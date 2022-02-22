e2c9a96a3b53941a45974e018825833bf3eacdc6
helloheesu <heesu.jung.developer@gmail.com>
2022-02-23 01:34:46 +0900

    merge page comp into page

--- src/pages/[character].tsx
+++ src/pages/[character].tsx
@@ -1,4 +1,9 @@
-import ResultPage from 'components/pages/Result';
+import Credits from 'components/Credits';
+import Head from 'components/Head';
+import Link from 'components/Link';
+import MainImage from 'components/MainImage';
+import Product from 'components/Product';
+import Share from 'components/Share';
 import { Character, characterNames, characters } from 'data/character';
 import { Locale } from 'data/languages';
 import results from 'data/results';
@@ -48,14 +53,61 @@ const Page: NextPage<Props> = ({ character, locale }: Props) => {
   const { descriptions, summary } = results[character];
   const characterName = characterNames[character][locale];
 
+  const imgSrc = `/images/characters/${character}.png`;
+  const title = {
+    ko: '나랑 잘 맞는 여행 친구는',
+    en: 'You best travel mate',
+  }[locale];
+
   return (
-    <ResultPage
-      character={character}
-      characterName={characterName}
-      descriptions={descriptions[locale]}
-      summary={summary[locale]}
-      locale={locale}
-    />
+    <div className="content-wrapper content-aligner result">
+      <Head
+        title={`${title}: ${characterName}`}
+        description={summary[locale]}
+        img={imgSrc}
+        pathname={character}
+      />
+      <section>
+        <header className="heading">
+          <p className="subtitle">{title}:</p>
+          <h1 className="title">{characterName}</h1>
+        </header>
+        <main className="main">
+          <div
+            style={{
+              position: 'relative',
+            }}
+          >
+            <MainImage src={imgSrc} alt={character} />
+            <div
+              style={{
+                position: 'absolute',
+                right: '1em',
+                top: '1em',
+              }}
+            >
+              {/* <LocaleToggler currentLocale={locale} /> */}
+            </div>
+          </div>
+          <div className="description">
+            <ul>
+              {descriptions[locale].map((text, i) => (
+                <li key={i}>{text}</li>
+              ))}
+            </ul>
+          </div>
+        </main>
+        <footer className="footer">
+          <Link
+            text={{ ko: '테스트 다시하기', en: 'Restart' }[locale]}
+            href={'/'}
+          />
+          <Share />
+        </footer>
+      </section>
+      <Product locale={locale} />
+      <Credits locale={locale} />
+    </div>
   );
 };
 
