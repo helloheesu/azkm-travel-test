b5fa9fd5b02f39544f3e66fc358f274a1ea85ac7
helloheesu <heesu.jung.developer@gmail.com>
2022-03-03 21:09:06 +0900

    preload result image

--- src/components/ImagePreLoader.tsx
+++ src/components/ImagePreLoader.tsx
@@ -2,12 +2,18 @@ import Image from 'next/image';
 
 interface Props {
   src: React.ComponentProps<typeof Image>['src'];
+  size?: { width: number; height: number };
 }
-const ImagePreLoader = ({ src }: Props) => {
+const ImagePreLoader = ({ src, size }: Props) => {
   return (
     <div className="preloader">
       {typeof src === 'string' ? (
-        <Image src={src} width={2050} height={1450} alt="" />
+        <Image
+          src={src}
+          width={size?.width || 2050}
+          height={size?.height || 1450}
+          alt=""
+        />
       ) : (
         <Image src={src} alt="" />
       )}
