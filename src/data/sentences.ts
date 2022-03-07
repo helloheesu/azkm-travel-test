403fc5816daa27ee53e765622f1365e80a06d7d7
helloheesu <heesu.jung.developer@gmail.com>
2022-03-07 23:01:59 +0900

    add translation to matches

--- src/data/sentences.ts
+++ src/data/sentences.ts
@@ -10,11 +10,11 @@ export const credits: { [key: string]: i18nText } = {
 export const product: { [key: string]: i18nText<string | string[]> } = {
   see_more: {
     ko: '아찌끄미 친구들의 해외 여행 중...',
-    en: 'See more about AZKM',
+    en: 'While traveling with AZKM friends...',
   },
   outro: {
     ko: ['아찌끄미 친구들이', '어떤 디저트를', '먹었는지 궁금하다면?'],
-    en: ["Let's see", 'what dessert', 'AZKM freinds', 'have chosen...'],
+    en: ["Let's see", 'which dessert', 'AZKM freinds', 'have chosen...'],
   },
 };
 
@@ -22,7 +22,7 @@ export const result: { [key: string]: i18nText } = {
   loading: { ko: '분석 중...', en: 'Loading...' },
   title: { ko: '나랑 잘 맞는 여행 친구는', en: 'You best travel mate' },
   restart: { ko: '테스트 다시하기', en: 'Restart' },
-  save: { ko: '꾹 눌러서 저장하기', en: 'Longpress to save' },
+  save: { ko: '꾹 눌러서 저장하기', en: 'Long Press to Save' },
   best_match: { ko: '찰떡친구', en: 'Best Match' },
   worst_match: { ko: '개떡친구', en: 'Worst Match' },
 };
