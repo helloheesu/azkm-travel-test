b7669eb9dfeda3792f337d206d978eb3523c375f
helloheesu <heesu.jung.developer@gmail.com>
2022-03-09 20:19:22 +0900

    redirect from old name to new name

--- next.config.js
+++ next.config.js
@@ -1,10 +1,27 @@
 /** @type {import('next').NextConfig} */
+
+const oldCharacterNameMap = {
+  hiff: 'hief',
+  cinamon: 'cinnamong',
+  coby: 'kobi',
+};
+const redirectList = Object.entries(oldCharacterNameMap).map(
+  ([oldName, newName]) => ({
+    source: `/${oldName}`,
+    destination: `/${newName}`,
+    permanent: true,
+  })
+);
+
 const nextConfig = {
   reactStrictMode: true,
   i18n: {
     locales: ['ko', 'en'],
     defaultLocale: 'ko',
     localeDetection: false,
+  },
+  async redirects() {
+    return redirectList;
   },
 };
 
